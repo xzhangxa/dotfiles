df01f0cd0b49e73f66187cb2fbde553ef8571ac4
Xin Zhang <zhangxin.xa@gmail.com>
2016-12-19 16:14:20 +0800

    Fix wrong commands

--- setup_my_shell.sh
+++ setup_my_shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "=== Setup proxy ==="
-if [ -z "$1" ]; then
+if [ -n "$1" ]; then
 	export http_proxy=$1
 	export https_proxy=$1
 	export HTTP_PROXY=$1
@@ -19,6 +19,7 @@ sudo apt-get install aptitude openssh-server git tmux vim vim-nox zsh curl inden
 
 echo "=== Setup oh-my-zsh ==="
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sed /env\ zsh/d)"
+sudo chsh -s /bin/zsh xzhang84
 
 echo "=== Download config files ==="
 git clone https://github.com/zhang-xin/my-config-files.git
@@ -30,9 +31,9 @@ cp my-config-files/.dir_colors ~
 rm -rf my-config-files
 
 echo "=== Setup Vundle and vim plugins ==="
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-sudo apt-get install libpython-dev
+sudo apt-get install libpython3-dev
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 
