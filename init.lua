0b20764ca71ba248abf2e50afa69c3d4d2cffc5a
Xin Zhang <zhangxin.xa@gmail.com>
2023-03-03 17:25:10 +0800

    nvim: recover wronly commented nvim cmdline completion

--- init.lua
+++ init.lua
@@ -195,7 +195,7 @@ cmp.setup.cmdline({'/', '?'}, {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  -- mapping = cmp.mapping.preset.cmdline(),
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
