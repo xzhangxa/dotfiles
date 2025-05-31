7674ba3be54e957a825e41881188fdb47fbcd275
Xin Zhang <zhangxin.xa@gmail.com>
2025-05-31 20:37:37 +0800

    setup: update kernel build dependency

--- kernel_dev.sh
+++ kernel_dev.sh
@@ -1,7 +1,7 @@
 # A few packages needed for kernel build and/or BPF related tools.
 
 # for kernel itself
-sudo apt install -y flex bison libssl-dev libelf-dev libncursesw5-dev debhelper
+sudo apt install -y flex bison libssl-dev libelf-dev debhelper libdw-dev
 
 # for tools/bpf, tools/lib/bpf, BTF
 sudo apt install -y binutils-dev libreadline-dev libcap-dev clang llvm pahole
