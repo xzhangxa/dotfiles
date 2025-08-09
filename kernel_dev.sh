69f541c7813e5ff8ce14170de1b1e32272b6107d
Xin Zhang <zhangxin.xa@gmail.com>
2025-08-09 11:56:53 +0800

    setup: add meson pkgconf

--- kernel_dev.sh
+++ kernel_dev.sh
@@ -5,3 +5,7 @@ sudo apt install -y flex bison gawk libssl-dev libelf-dev debhelper libdw-dev
 
 # for tools/bpf, tools/lib/bpf, BTF
 sudo apt install -y binutils-dev libreadline-dev libcap-dev clang llvm pahole
+
+# if use Ubuntu default kernel config
+# ./scripts/config --disable SYSTEM_TRUSTED_KEYS
+# ./scripts/config --disable SYSTEM_REVOCATION_KEYS
