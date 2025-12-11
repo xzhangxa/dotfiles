f1f6906d93bf0a9ee15b4a4888554838637985ae
Xin Zhang <zhangxin.xa@gmail.com>
2025-12-11 15:06:34 +0800

    nvim: nvim-tree auto focus current file

--- init.lua
+++ init.lua
@@ -162,6 +162,12 @@ require("nvim-tree").setup({
       close = true,
     },
   },
+  update_focused_file = {
+    enable = true,
+    update_root = {
+      enable = true,
+    },
+  },
 })
 vim.keymap.set('n', '<leader>F', ':NvimTreeFindFile!<cr>', {})
 
